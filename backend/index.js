3aeb163a5cc94d1ef151b1ef3cb91cc8abe30d68
LuisM <muislarques@gmail.com>
2025-05-14 21:11:48 +0100

    fixed frontend didnt get the database information

--- backend/index.js
+++ backend/index.js
@@ -12,16 +12,16 @@ app.use(express.json());
 
 // middleware to handle CORS POLICY (Cross-Origin Resource Sharing)
 // option1: allow all origins with default of cors(*)
-//app.use(cors());
+app.use(cors());
 
 // option2: allow custom origins
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET","POST","PUT","DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET","POST","PUT","DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get('/', (request, response) => {
   console.log(request)
