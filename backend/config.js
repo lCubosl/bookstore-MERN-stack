701a2908d555d0cd16460062884b157926311e9a
lCubosl <141191881+lCubosl@users.noreply.github.com>
2025-05-19 16:06:59 +0100

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = "mongodb+srv://root:YC0hvLDEvFT9Ht0Q@bookstore-mern.k7ppsq5.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN"
-
